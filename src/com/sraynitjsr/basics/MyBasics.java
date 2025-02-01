442cc281c1b6d57b46d1ad1d1724ef4af09525f2
Subhradeep Ray <subhradeepray2017@gmail.com>
2025-02-01 11:57:46 +0530

    Basics of Java

---
+++ src/com/sraynitjsr/basics/MyBasics.java
@@ -0,0 +1,7 @@
+package com.sraynitjsr.basics;
+
+public class MyBasics {
+	public static void start() {
+		System.out.println("Java Basics");
+	}
+}
--- src/com/sraynitjsr/main/MyMain.java
+++ src/com/sraynitjsr/main/MyMain.java
@@ -1,7 +1,12 @@
 package com.sraynitjsr.main;
 
+import com.sraynitjsr.basics.MyBasics;
+
 public class MyMain {
 	public static void main(String[] args) {
 		System.out.println("DSA Using Core Java");
+		
+		System.out.println();
+		MyBasics.start();
 	}
 }

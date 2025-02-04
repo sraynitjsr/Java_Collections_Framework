3033fa137c4beb1cc0533feb867c421770a971ba
Subhradeep Ray <subhradeepray2017@gmail.com>
2025-02-05 03:43:27 +0530

    Arrays in Java

--- src/com/sraynitjsr/main/MyMain.java
+++ src/com/sraynitjsr/main/MyMain.java
@@ -3,21 +3,25 @@ package com.sraynitjsr.main;
 import com.sraynitjsr.basics.*;
 import com.sraynitjsr.conditionalstatements.*;
 import com.sraynitjsr.loops.*;
+import com.sraynitjsr.arrays.*;
 
 public class MyMain {
 	public static void main(String[] args) {
 		System.out.println("DSA Using Core Java");
 		
-		System.out.println();
-		MyBasics.start();
+//		System.out.println();
+//		MyBasics.start();
+//		
+//		System.out.println();
+//		MyDataTypesVariables.start();
+//		
+//		System.out.println();
+//		MyConditionalStatements.start();
+//		
+//		System.out.println();
+//		MyLoops.start();
 		
 		System.out.println();
-		MyDataTypesVariables.start();
-		
-		System.out.println();
-		MyConditionalStatements.start();
-		
-		System.out.println();
-		MyLoops.start();
+		MyArrays.start();		
 	}
 }

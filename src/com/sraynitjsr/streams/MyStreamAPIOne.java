758edde859aad226a72078ce5ffc16ccc75c1e35
Subhradeep Ray <subhradeepray2017@gmail.com>
2025-02-13 23:23:03 +0530

    Stream API 1

--- src/com/sraynitjsr/streams/MyStreamAPIOne.java
+++ src/com/sraynitjsr/streams/MyStreamAPIOne.java
@@ -1,7 +1,23 @@
 package com.sraynitjsr.streams;
 
+import java.util.*;
+import java.util.stream.*;
+
 public class MyStreamAPIOne {
 	public static void start() {
 		System.out.println("Streams API Using Java");
+
+		ArrayList<Integer> myList = new ArrayList<>(Arrays.asList(1, 3, 1, 2, -5, 1, 3, 4));
+		
+		System.out.println(myList);
+		
+		Stream<Integer> stream1 = myList.stream();
+		
+		stream1.forEach(data -> System.out.print(data + " "));
+		System.out.println();
+		
+		Stream<Integer> stream2 = myList.stream();
+		stream2.forEach(System.out::print);
+		System.out.println();
 	}
 }

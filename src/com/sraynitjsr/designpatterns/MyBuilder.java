c55cfc7d044b6a6830bd299d37222b0fb2e098f4
Subhradeep Ray <subhradeepray2017@gmail.com>
2025-02-16 18:45:36 +0530

    Builder Design Pattern

--- src/com/sraynitjsr/designpatterns/MyBuilder.java
+++ src/com/sraynitjsr/designpatterns/MyBuilder.java
@@ -1,7 +1,80 @@
 package com.sraynitjsr.designpatterns;
 
-public class MyBuilder {
-	public static void start() {
-		
+class Book {
+	private String title;
+	private String author;
+	private double price;
+	private String genre = "Not Provided";
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getBookDetails() {
+		return "Title: " + title + ", Author: " + author + ", Price: " + price + (genre != null ? ", Genre: " + genre : "");
 	}
 }
+
+class BookBuilder {
+	private Book book;
+
+	public BookBuilder() {
+		book = new Book();
+	}
+
+	public BookBuilder addTitle(String title) {
+		book.setTitle(title);
+		return this;
+	}
+
+	public BookBuilder addAuthor(String author) {
+		book.setAuthor(author);
+		return this;
+	}
+
+	public BookBuilder addPrice(double price) {
+		book.setPrice(price);
+		return this;
+	}
+
+	public BookBuilder addGenre(String genre) {
+		book.setGenre(genre);
+		return this;
+	}
+
+	public Book build() {
+		return book;
+	}
+}
+
+public class MyBuilder {
+	public static void start() {
+		Book book1 = new BookBuilder()
+				.addTitle("GoLang")
+				.addAuthor("Subhradeep Ray")
+				.addPrice(999.999)
+				.build();
+		System.out.println(book1.getBookDetails());
+
+		Book book2 = new BookBuilder()
+				.addTitle("Core and Advanced Java")
+				.addAuthor("I Am Sray")
+				.addPrice(111.111)
+				.addGenre("Technical")
+				.build();
+		System.out.println(book2.getBookDetails());
+	}
+}
+
